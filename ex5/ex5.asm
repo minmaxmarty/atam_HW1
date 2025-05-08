4817b900d31bc63d67ddcaddcddc8f297f14f7a5
minmaxmarty <aregmkhitaryan@gmail.com>
2025-05-08 14:18:53 +0300

    add ex5 initial idea

--- ex5/ex5.asm
+++ ex5/ex5.asm
@@ -1,4 +1,33 @@
 .global _start
 
+# used registers: 
+
 .section .text
 _start:
+    movq String(%rip), %rax # rax = &String[0]
+    movq $0, %rsi # i = 0 (index)
+start_loop_HW1:
+    movq (%rax, %rsi, 1), %rbx
+    cmpb $'a', %bl
+    je check_add_HW1
+    cmpb $'s', %bl
+    je check_sub_HW1
+    cmpb $'m', %bl 
+    je check_mul_HW1
+    cmpb $'d', %bl
+    je check_div_HW1
+    cmpb $'i', %bl
+    je continue_HW1
+    jmp check_if_number_HW1
+
+
+
+check_if_number_HW1:
+    
+
+continue_HW1:
+    incq %rsi
+    jmp start_loop_HW1
+
+    
+    
